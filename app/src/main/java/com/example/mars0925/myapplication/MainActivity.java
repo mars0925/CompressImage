5f8d1eb1f82a226bcd2e8e2cdd6fb6cd54d7f410
Mars <mars0925@24drs.com>
2018-06-08 17:34:26 +0800

    再度聯手保羅？傳小喬丹盼加盟火箭

--- app/src/main/java/com/example/mars0925/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mars0925/myapplication/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt1.setOnClickListener(this);
         bt2.setOnClickListener(this);
 
-
+        //再度聯手保羅？傳小喬丹盼加盟火箭
     }
 
     @Override
